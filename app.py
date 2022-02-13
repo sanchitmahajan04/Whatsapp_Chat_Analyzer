326be53f94ac5ec8a6297e3fa7c25e4d824625c9
sanchit <sanchitmahajan2001@gmail.com>
2022-02-14 00:38:41 +0530

    make it better

--- app.py
+++ app.py
@@ -118,13 +118,14 @@ if uploaded_file is not None:
 
         # emoji analysis
         emoji_df = helper.emoji_helper(selected_user,df)
-        st.title("Emoji Analysis")
+        if emoji_df.empty==False:
+            st.title("Emoji Analysis")
 
-        col1,col2 = st.columns(2)
+            col1,col2 = st.columns(2)
 
-        with col1:
-            st.dataframe(emoji_df)
-        with col2:
-            fig,ax = plt.subplots()
-            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
-            st.pyplot(fig)
+            with col1:
+                st.dataframe(emoji_df)
+            with col2:
+                fig,ax = plt.subplots()
+                ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct="%0.2f")
+                st.pyplot(fig)
